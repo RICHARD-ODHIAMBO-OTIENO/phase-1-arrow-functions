0b9fae41656bbfb618e954a585ac007b2ebb0580
Richard Odhiambo Otieno <kotienor1@gmail.com>
2024-03-31 09:35:11 +0300

    Arrow function

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function() {
+    return 2000/100;
+}
+
+// An arrow fuction called square
+const square = (side) => {
+    return side ** 2;
+}
+
+// Create an arrow function called add
+const add = (length1, length2) => {
+    return length1 + length2
+}
